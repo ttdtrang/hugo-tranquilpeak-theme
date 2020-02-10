3633925300034718b8035e4a5b300f79a69f2b35
Trang Tran <ttdtrang@gmail.com>
2020-02-09 17:33:08 -0700

    update theme

--- src/js/math-code.js
+++ src/js/math-code.js
@@ -1,6 +1,6 @@
 // Written by Yihui
 (function() {
-  var i, text, code, codes = document.getElementsByTagName('code');
+  let i, text, code, codes = document.getElementsByTagName('code');
   for (i = 0; i < codes.length;) {
     code = codes[i];
     if (code.parentNode.tagName !== 'PRE' && code.childElementCount === 0) {
